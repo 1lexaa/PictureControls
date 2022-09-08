fc55304ba2e45f2f9fa99b0ccf0073d224191394
1lexaa <nice140@icloud.com>
2022-09-08 10:33:13 +0300

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace PictureControls
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        public Form()
+        {
+            InitializeComponent();
+        }
+    }
+}
